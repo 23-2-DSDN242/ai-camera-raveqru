b8855078b0e56623f639ea37884ecab0a47b1763
Anna Giniiatullina <131623439+raveqru@users.noreply.github.com>
2024-05-21 11:03:35 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,11 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_7.png"
+// "input_2.png"]
+
+let maskFile   = "mask_7.png" //"mask_2.png"]
+let outputFile = "output_4.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,7 +20,7 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -30,17 +32,29 @@ function draw () {
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
+
+  
     if(mask[0] > 128) {
+      fill(54,103,53)
       let pointSize = 10;
       ellipse(x, y, pointSize, pointSize);
+      let pointSize1 = 3;
+      fill(103,145,103)
+      ellipse (x, y, pointSize1, pointSize1);
+      if (random() < 0.07) { // Adjust this  as needed
+        fill(255,255,0); // Yellow color
+        let pointSize1 = 7;
+        ellipse(x, y, pointSize1, pointSize1);
+      }
     }
+    
     else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+      let pointSize = 10;
+      ellipse(x, y, pointSize, pointSize);    
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 25) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
